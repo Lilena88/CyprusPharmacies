032e44fd79265580c14df9c1eb9f66211e426121
Елена Ким <ekim@htc-cs.ru>
2023-06-12 16:21:58 +0300

    fix folder

--- CyprusPharmacies/Resources/AppearanceSources.swift
+++ CyprusPharmacies/Resources/AppearanceSources.swift
@@ -15,7 +15,10 @@ enum AppearanceSource {
     static let currentLocationButtonBottom: CGFloat = 50
     static let infoPopoverSize: CGSize = CGSize(width: 300, height: 360)
     static let dateLabelSize: CGSize = CGSize(width: 100, height: 30)
-
+    
     static let infoImage: UIImage? = UIImage(systemName: "info.circle", withConfiguration:  UIImage.SymbolConfiguration(pointSize: 16, weight: .light, scale: .large))
     static let pharmacyPin: UIImage? = UIImage(systemName: "cross.circle", withConfiguration:  UIImage.SymbolConfiguration(pointSize: 15, weight: .light, scale: .large))?.withRenderingMode(.alwaysTemplate).colorized(color: .systemGreen)
+        
+    static let clusterIcon = UIImage(named: "Cluster")
+    
 }
